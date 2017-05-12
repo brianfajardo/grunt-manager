d25bd4d072d5a4a41d3468a792ee09ec863cc93b
Brian Fajardo <bmf5@hotmail.com>
2017-05-12 14:12:25 -0400

    Enable Firebase email and password login

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import firebase from 'firebase'
+import firebaseConfig from './firebaseConfig'
 import rootReducer from './src/reducers'
 
-export default App = () =>
-  <Provider store={createStore(rootReducer)}>
-    <View style={styles.container}>
-      <Text>Employer Simulator 2017</Text>
-    </View>
-  </Provider>
+export default class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp(firebaseConfig)
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(rootReducer)}>
+        <View style={styles.container}>
+          <Text>Employer Simulator 2017</Text>
+        </View>
+      </Provider>
+    )
+  }
+}
+
 
 const styles = StyleSheet.create({
   container: {
