aa96dd14e6852b2dbdc83aa447d0c55ea4fceb4b
Brian Fajardo <bmf5@hotmail.com>
2017-05-17 18:11:55 -0400

    Create employeeDelete action creator to remove employee from database

--- src/actions/employeesActions.js
+++ src/actions/employeesActions.js
@@ -52,3 +52,12 @@ export const employeeSave = (name, phone, shift, uid) => {
       })
   }
 }
+
+export const employeeDelete = uid => {
+  const { currentUser } = firebase.auth()
+  return () => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+      .remove()
+      .then(() => Actions.employees({ type: 'reset' }))
+  }
+}
--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Card, Button, Divider } from 'react-native-elements'
-import { employeeUpdate, employeeSave } from '../actions/employeesActions'
+import { Card, Button } from 'react-native-elements'
+import * as actions from '../actions/employeesActions'
 import _ from 'lodash'
 import PropTypes from 'prop-types'
 import Communications from 'react-native-communications'
@@ -15,6 +15,9 @@ class EmployeeEdit extends Component {
     this.state = { showModal: false }
     this.onSavePress = this.onSavePress.bind(this)
     this.onTextPress = this.onTextPress.bind(this)
+    this.onFirePress = this.onFirePress.bind(this)
+    this.onAcceptPress = this.onAcceptPress.bind(this)
+    this.onDeclinePress = this.onDeclinePress.bind(this)
   }
 
   // Iterate over every prop of the employee provided
@@ -44,18 +47,31 @@ class EmployeeEdit extends Component {
     Communications.text(phone, `Your upcoming shift is on ${shift}`)
   }
 
+  onFirePress() {
+    this.setState({ showModal: true })
+  }
+
+  onAcceptPress() {
+    const { employeeDelete, employee: { uid } } = this.props
+    employeeDelete(uid)
+  }
+
+  onDeclinePress() {
+    this.setState({ showModal: false })
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm {...this.props} />
         <Button title="Update Changes" onPress={this.onSavePress} />
         <Button title="Text Schedule" onPress={this.onTextPress} />
-        <Button title="Fire" onPress={
-          () => this.setState({ showModal: !this.state.showModal })}
-        />
+        <Button title="Fire" onPress={this.onFirePress} />
         <ConfirmModal
           visible={this.state.showModal}
           title="Are you sure you want to fire this employee?"
+          onAcceptPress={this.onAcceptPress}
+          onDeclinePress={this.onDeclinePress}
         />
       </Card>
     )
@@ -78,10 +94,10 @@ EmployeeEdit.propTypes = {
   name: PropTypes.string.isRequired,
   phone: PropTypes.string.isRequired,
   shift: PropTypes.string.isRequired,
+  // actions
   employeeUpdate: PropTypes.func.isRequired,
-  employeeSave: PropTypes.func.isRequired
+  employeeSave: PropTypes.func.isRequired,
+  employeeDelete: PropTypes.func.isRequired
 }
 
-export default connect(mapStateToProps, {
-  employeeUpdate, employeeSave
-})(EmployeeEdit)
+export default connect(mapStateToProps, actions)(EmployeeEdit)
